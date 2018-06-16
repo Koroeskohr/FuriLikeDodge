53442ee48dcc45cd009b4db244b559c7f8a18b36
Victor Viale <somebodywas@gmail.com>
2018-06-16 04:14:04 +0200

    Add normalized vector structure

---
+++ Assets/Scripts/lib/utils/NormalizedVector.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace FuriLikeDodge
+{
+  public struct NormalizedVector
+  {
+    private readonly Vector3 _value;
+
+    public NormalizedVector(Vector3 v)
+    {
+      _value = v.normalized;
+    }
+
+    public Vector3 Value { get { return _value; } }
+    public override string ToString()
+    {
+      return _value.ToString();
+    }
+  }
+}
